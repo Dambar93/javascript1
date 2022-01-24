1ab69ad7eab31101b91d074e47db39bece2e939b
Dambar93 <97044258+Dambar93@users.noreply.github.com>
2022-01-24 17:50:49 +0200

    ikeliam2

---
+++ dist/js/script.js
@@ -0,0 +1,81 @@
+"use strict";
+const arr = [
+  {
+    name: 'blue',
+    file_type:'.png' ,
+    size: 77.7,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'family',
+    file_type:'.png' ,
+    size: 55.9,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'fish',
+    file_type:'.png' ,
+    size: 33.5,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'friends',
+    file_type:'.png' ,
+    size: 47.6,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'grass',
+    file_type:'.png' ,
+    size: 42.8,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'gymnast',
+    file_type:'.png' ,
+    size: 88.4,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'kid',
+    file_type:'.png' ,
+    size: 57.5,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'sunset',
+    file_type:'.png' ,
+    size: 24.8,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'tulip',
+    file_type:'.png' ,
+    size: 28.6,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'waitin',
+    file_type:'.png' ,
+    size: 65.1,
+    measurment: 'kB'
+
+  },
+  {
+    name: 'wonderful',
+    file_type:'.png' ,
+    size: 89.4,
+    measurment: 'kB'
+
+  }
+]
+console.log(arr);
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const {
+  src,
+  dest,
+  series,
+  watch,
+} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const include = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+const del = require('del');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const sync = require('browser-sync').create();
+const gulpFont=require('gulp-font');
+
+
+
+function html() {
+  return src('src/**.html')
+    .pipe(include({
+      prefix: '@@',
+    }))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(dest('dist'))
+}
+
+
+function scss() {
+  return src('src/styles/**.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(csso())
+    .pipe(concat('index.css'))
+    .pipe(dest('dist'))
+}
+function img(){
+  return src('src/img/*/**.{png,gif,jpg}')
+  .pipe(dest('dist/img'))
+}
+function img_folders(){
+  return src('src/img/**.{png,gif,jpg}')
+  .pipe(dest('dist/img'))
+}
+
+async function clear() {
+  del('dist');
+}
+function fonts(){
+  return src('src/font/*/**.{ttf,otf}')
+  .pipe(dest('dist/font'))
+}
+function js(){
+  return src('src/js/**.js')
+  .pipe(dest('dist/js'))
+}
+// function font() {
+//   return src('src/font/**/*.{ttf,otf}', { read: false })
+//       .pipe(gulpFont({
+//           ext: '.css',
+//           fontface: 'src/font',
+//           relative: '/font',
+//           dest: 'dist/font',
+//           embed: ['woff'],
+//           collate: false
+//       }))
+//       .pipe(dest('dist/font'));
+// }
+
+function serve() {
+  sync.init({
+    server: './dist'
+  })
+  watch('src/js/**.js', series(js)).on('change', sync.reload)
+  watch('src/**.html', series(html)).on('change', sync.reload)
+  watch('src/styles/**.scss', series(scss)).on('change', sync.reload)
+  watch('src/parts/**.html', series(html)).on('change', sync.reload)
+}
+
+
+exports.start = series(clear, html,img,img_folders, scss,js, serve);
+exports.reload = series(html,img, scss,img_folders,js, serve);
+
+exports.delete = series(clear);
